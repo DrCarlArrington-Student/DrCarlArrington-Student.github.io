fb815dfb67aa44eb6e07eef5f2b176de295feaca
DrCarlArrington-Student <90409410+DrCarlArrington-Student@users.noreply.github.com>
2024-07-16 16:44:14 -0400

    Update basicMap.js

--- basicMap.js
+++ basicMap.js
@@ -20,6 +20,8 @@ L.marker([35.2176665, -80.831473], {
 <hr>
 <a href="https://www.cpcc.edu">Click Here to Visit CPCC's Website</a><br>
 <iframe width="280" height="160" src="https://www.youtube.com/embed/wCyHWiYPiXU?si=l0Oct5IxY2BXYIxo" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+<hr>
+<p>[URL: https://www.youtube.com/watch?v=wCyHWiYPiXU]</p>
 </center>
 `)
   .addTo(map);
